f84b3f523547f0e557725577aae31c98981d46d9
Marcelo Knust <49623197+Marceloknust@users.noreply.github.com>
2022-07-26 12:18:10 -0300

    Update Adivinum.py

--- Adivinum.py
+++ Adivinum.py
@@ -8,8 +8,7 @@ def primo(numero):
     for num in range(1, numero + 1):
         if numero % num == 0:
             contador += 1
-    print('Esse número é divisivel por {} outros numeros!'.format(contador))
-
+    return contador
 
 def organizador(o):
     for j in range(len(o)):
@@ -25,42 +24,42 @@ def jogo():
     cont = 0
     denovo = "s"
     certo = False
-    chutes = [1]
+    acertos = [1]
     errados = [0]
     nome = str(input('Bom dia, vamos jogar! Qual seu nome? '))
     while nome == "":  # impedidno que o usuário coloque um nome em branco
         nome = input("Por favor, Digite um nome válido: ")
     system("cls")
-    print('''Olá {}, vamos jogar um jogo de adivinhação matemática?
+    print(f'''Olá {nome}, vamos jogar um jogo de adivinhação matemática?
 Como funciona o jogo, eu escolhi um nùmero entre 0 e 1000 e você tem que adivinhar esse número.
-Mas calma temos algumas dicas para te ajudar...'''.format(nome))
+Mas calma temos algumas dicas para te ajudar...''')
     print('-=-' * 30)
     print('''Você podera chutar um número qualquer e eu te direi se é o numero escolhido ou não.
 Se não for, direi se meu numero é ou não divisivel pelo numero escolhido.
-Vamos começar?'''.format(nome))
+Vamos começar?''')
     print('Vamos as primeiras dicas')
     while certo == False and denovo == "s":
-        print('O número escolhido é divisivel por {}!'.format(chutes))
-        print('E também não é divisivel por {}, interessante!'.format(errados))
-        primo(n) # identificar quantos divisores tem
-        c = input('Digite o {}º chute:'.format(cont + 1)).strip()  # impedindo caracteres em branco no chute
+        print(f'O número escolhido é divisivel por {acertos}!')
+        print(f'E também não é divisivel por {errados}, interessante!')
+        print(f'Esse número é divisivel por {primo(n)} outros numeros!')
+        c = input(f'Digite o {cont + 1}º chute:').strip()  # impedindo caracteres em branco no chute
         while c.isnumeric() is False:
             c = input('Por favor, digite um numero válido: ').lower().strip()
         c = int(c)
         if n == c:
             system("cls")
             print('Caraca, você acertou, parabéns!')
-            print('Você utilizou {} tentativas para acertar!'.format(cont + 1))
+            print(f'Você utilizou {cont + 1} tentativas para acertar!')
             certo = True
         else:
             system("cls")
             print('Que pena, não era esse número!')
             if (n % c) == 0:
-                chutes.append(c)
+                acertos.append(c)
                 cont = cont + 1
-                print('Porém o meu número escolhido é sim divisivel por {}'.format(c))
+                print(f'Porém o meu número escolhido é sim divisivel por {c}')
             else:
-                print('O número também não é divisivel por {}'.format(c))
+                print(f'O número também não é divisivel por {c}')
                 errados.append(c)
                 cont = cont + 1
             if (cont % 5) == 0:
@@ -70,10 +69,11 @@ Vamos começar?'''.format(nome))
                     tentar = str(input("Digite uma opção válida? (S/N)").strip().lower())
                 if tentar == 'n':
                     novamente()
-        print(len(chutes))
-        organizador(chutes)
+        acertos.sort()
+        print(acertos)
+        print(n)
 
-    print('Meu número era {}'.format(n))
+    print(f'Meu número era {n}')
     novamente()
 
 
